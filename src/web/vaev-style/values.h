7ab3e27dd14f9d1a8b468884d861479e24aadc60
Paulo Medeiros <palm@odoo.com>
2024-11-05 12:47:45 +0100

    style: add break props

--- src/web/vaev-style/values.h
+++ src/web/vaev-style/values.h
@@ -2,6 +2,7 @@
 
 #include <vaev-base/align.h>
 #include <vaev-base/borders.h>
+#include <vaev-base/break.h>
 #include <vaev-base/calc.h>
 #include <vaev-base/color.h>
 #include <vaev-base/display.h>
@@ -70,6 +71,16 @@ struct ValueParser<BorderCollapse> {
 template <>
 struct ValueParser<BorderSpacing> {
     static Res<BorderSpacing> parse(Cursor<Css::Sst> &c);
+};
+
+template <>
+struct ValueParser<BreakBetween> {
+    static Res<BreakBetween> parse(Cursor<Css::Sst> &c);
+};
+
+template <>
+struct ValueParser<BreakInside> {
+    static Res<BreakInside> parse(Cursor<Css::Sst> &c);
 };
 
 template <typename T>
