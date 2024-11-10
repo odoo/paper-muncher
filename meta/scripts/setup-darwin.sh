61393d03c436e4518b89e0d2adfbeb2f747ed94b
Akinimaginable <akn@etrange.org>
2024-11-10 16:57:41 +0100

    meta: Added brew checks, lld, pkgconfig and reordered packages (#8)
    * added brew checks, lld, pkgconfig and reordered packages
    
    * Added brew check

--- meta/scripts/setup-darwin.sh
+++ meta/scripts/setup-darwin.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-brew install nasm llvm sdl2 python3 ninja libmagic
+if ! command -v brew &> /dev/null
+then
+    echo "Homebrew is not installed. Please install Homebrew first."
+    echo '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
+    exit 1
+fi
+
+brew install libmagic lld llvm nasm ninja pkg-config python3 sdl2
