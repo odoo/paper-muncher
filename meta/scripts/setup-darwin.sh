fa9103f59658b303c6b4975507285eebe8b973b4
Akinimaginable <akn@etrange.org>
2024-11-10 16:50:07 +0100

    Added brew check

--- meta/scripts/setup-darwin.sh
+++ meta/scripts/setup-darwin.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
+if ! command -v brew &> /dev/null
+then
+    echo "Homebrew is not installed. Please install Homebrew first."
+    echo '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
+    exit 1
+fi
+
 brew install libmagic lld llvm nasm ninja pkg-config python3 sdl2
