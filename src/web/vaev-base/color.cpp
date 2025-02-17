daccf5d1a69c5b34f4047c6c8ef7374fe4f791a2
Paulo Medeiros <palm@odoo.com>
2025-02-17 15:01:12 +0100

    vaev-style: Add color-mix function for CSS.

--- src/web/vaev-base/color.cpp
+++ src/web/vaev-base/color.cpp
@@ -1,5 +1,7 @@
 #include "color.h"
 
+#include "karm-logger/logger.h"
+
 namespace Vaev {
 
 Opt<Color> parseNamedColor(Str name) {
@@ -44,6 +46,46 @@ Gfx::Color resolve(Color c, Gfx::Color currentColor) {
 
     case Color::Type::CURRENT:
         return currentColor;
+
+    case Color::Type::MIX: {
+        // https://developer.mozilla.org/en-US/docs/Web/CSS/color_value/color-mix#percentage
+        Color::ColorMix& cm = c._color.unwrap<Color::ColorMix>();
+
+        auto resolvedColorA = resolve(*cm.colorA, currentColor);
+        auto resolvedColorB = resolve(*cm.colorB, currentColor);
+
+        i8 percA, percB;
+
+        if (cm.percA == NONE and cm.percB == NONE) {
+            percA = percB = 50;
+        } else if (cm.percA == NONE and cm.percB) {
+            percB = cm.percB.unwrap();
+            percA = 100 - percB;
+        } else if (cm.percA and cm.percB == NONE) {
+            percA = cm.percA.unwrap();
+            percB = 100 - percA;
+        } else {
+            percA = cm.percA.unwrap();
+            percB = cm.percB.unwrap();
+        }
+
+        if (percA == percB and percA == 0) {
+            logWarn("cannot mix colors when both have zero percentages");
+            return Gfx::WHITE;
+        }
+
+        f64 normPercB = percB;
+
+        if (percA + percB != 100)
+            normPercB = normPercB / (percA + percB);
+        else
+            normPercB /= 100;
+
+        auto finalColor = cm.colorSpace.interpolate(resolvedColorA, resolvedColorB, normPercB);
+        if (percA + percB < 100)
+            finalColor.alpha = percA + percB;
+        return finalColor;
+    }
 
     default:
         panic("Invalid color type");
