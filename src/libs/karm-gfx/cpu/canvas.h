5d7c6279d7466859590724dfdc9b57e4fbac63b0
lmer-odoo <lmer@odoo.com>
2025-03-26 10:35:07 +0100

    karm-gfx: Add support for clipping in cpu renderer

--- src/libs/karm-gfx/cpu/canvas.h
+++ src/libs/karm-gfx/cpu/canvas.h
@@ -25,6 +25,7 @@ struct CpuCanvas : Canvas {
         Stroke stroke{};
         Math::Recti clip{};
         Math::Trans2f trans = Math::Trans2f::IDENTITY;
+        Opt<Rc<Surface>> clipMask = NONE;
     };
 
     Opt<MutPixels> _pixels{};
