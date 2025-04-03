333939f7c440df31d826348f07f548be6e61f8de
sleepy-monax <me@smnx.sh>
2025-04-03 08:45:44 +0200

    meta: Added missing libseccomp.

--- meta/scripts/setup-ubuntu.sh
+++ meta/scripts/setup-ubuntu.sh
@@ -3,5 +3,5 @@
 set -e
 
 apt-get update -y --no-install-recommends
-apt-get install -y --no-install-recommends build-essential git ninja-build libsdl2-dev nasm liburing-dev ccache jq
+apt-get install -y --no-install-recommends build-essential git ninja-build libsdl2-dev nasm liburing-dev ccache jq libseccomp-dev
 bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)" llvm 20 all
