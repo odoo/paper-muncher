4387f0ef7764d7d7e7c768e4135603961c07681a
Paulo Medeiros <palm@odoo.com>
2025-08-05 16:03:56 +0200

    vaev-engine: Expose padding, radii, size computation API as public.

--- src/vaev-engine/layout/layout.cpp
+++ src/vaev-engine/layout/layout.cpp
@@ -12,6 +12,12 @@ export InsetsAu computeMargins(Tree& tree, Box& box, Input input);
 
 export InsetsAu computeBorders(Tree& tree, Box& box);
 
+export InsetsAu computePaddings(Tree& tree, Box& box, Vec2Au containingBlock);
+
+export Math::Radii<Au> computeRadii(Tree& tree, Box& box, Vec2Au size);
+
+export Opt<Au> computeSpecifiedSize(Tree& tree, Box& box, Size size, Vec2Au containingBlock, bool isWidth);
+
 export Vec2Au computeIntrinsicSize(Tree& tree, Box& box, IntrinsicSize intrinsic, Vec2Au containingBlock);
 
 export Output layout(Tree& tree, Box& box, Input input);
