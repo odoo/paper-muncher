da41c875202774bd17dcc402b7955640abc946ac
sleepy-monax <me@smnx.sh>
2025-10-24 11:49:01 +0200

    wip

--- src/vaev-webdriver/main.cpp
+++ src/vaev-webdriver/main.cpp
@@ -1,15 +1,14 @@
+#include <karm-core/macros.h>
 #include <karm-sys/entry.h>
 
 import Karm.Core;
 import Karm.Http;
+import Karm.Cli;
 import Vaev.Engine;
 
 using namespace Karm;
 
 namespace Vaev::Webdriver {
-
-struct Uuid {
-};
 
 // MARK: 6. Protocol -----------------------------------------------------------
 // https://www.w3.org/TR/webdriver2/#protocol
@@ -26,11 +25,12 @@ struct ReadinessState {
 };
 
 struct Session {
+    Ref::Uuid uuid;
     Rc<Dom::Window> window;
 };
 
 struct Webdriver {
-    Map<Uuid, Session> _sessions;
+    Map<Ref::Uuid, Rc<Session>> _sessions;
 
     // MARK: 7. Capabilities ---------------------------------------------------
 
@@ -38,13 +38,29 @@ struct Webdriver {
     // https://www.w3.org/TR/webdriver2/#sessions
 
     // https://www.w3.org/TR/webdriver2/#new-session
-    Res<Uuid> newSession();
+    Res<Ref::Uuid> newSession() {
+        auto uuid = try$(Ref::Uuid::v4());
+        _sessions.put(
+            uuid,
+            makeRc<Session>(
+                uuid,
+                Dom::Window::create(Http::defaultClient())
+            )
+        );
+        return Ok(uuid);
+    }
 
     // https://www.w3.org/TR/webdriver2/#delete-session
-    Res<> deleteSession();
+    Res<> deleteSession(Ref::Uuid uuid) {
+        _sessions.del(uuid);
+    }
 
     // https://www.w3.org/TR/webdriver2/#status
-    Res<ReadinessState> status();
+    Res<ReadinessState> status() {
+        return Ok(ReadinessState{
+            .ready = true,
+        });
+    }
 
     // MARK: 9. Timeout --------------------------------------------------------
     // https://www.w3.org/TR/webdriver2/#timeouts
@@ -59,10 +75,16 @@ struct Webdriver {
     // https://www.w3.org/TR/webdriver2/#navigation
 
     // https://www.w3.org/TR/webdriver2/#navigate-to
-    Res<> navigateTo();
+    Async::Task<> navigateTo(Ref::Uuid sessionId, Ref::Url url) {
+        auto session = co_try$(_sessions.tryGet(sessionId));
+        co_return session->window->loadLocationAsync(url);
+    }
 
     // https://www.w3.org/TR/webdriver2/#get-current-url
-    Res<> getCurrentUrl();
+    Res<Ref::Url> getCurrentUrl(Ref::Uuid sessionId) {
+        auto session = try$(_sessions.tryGet(sessionId));
+        return Ok(session->window->location());
+    }
 
     // https://www.w3.org/TR/webdriver2/#back
     Res<> back();
@@ -123,19 +145,43 @@ struct Webdriver {
 
 } // namespace Vaev::Webdriver
 
-Async::Task<> entryPointAsync(Sys::Context&) {
+Async::Task<> entryPointAsync(Sys::Context& ctx) {
+    auto portOption = Cli::option<isize>('p', "port"s, "TCP port to listen to."s);
+    Cli::Section serverSection = {"Server Options"s, {portOption}};
+
+    Cli::Command cmd{
+        "vaev-webdriver"s,
+        "Webdriver protocol implementation for vaev."s,
+        {serverSection}
+    };
+
+    co_trya$(cmd.execAsync(ctx));
+    if (not cmd)
+        co_return Ok();
+
     auto webdriver = makeRc<Vaev::Webdriver::Webdriver>();
     auto router = makeRc<Http::Router>();
 
-    router->route("GET /debug", [webdriver](Rc<Http::Request>, Rc<Http::Response::Writer>) -> Async::Task<> {
+    router->route("GET /", [webdriver](Rc<Http::Request>, Rc<Http::Response::Writer> resp) -> Async::Task<> {
+        co_trya$(resp->writeStrAsync(R"html(
+            <!DOCTYPE html>
+            <html>
+                <body>
+                    <h1>It works!</h1>
+                    <p>Vaev WebDriver</p>
+                </body>
+            </html>
+        )html"s));
         co_return Ok();
     });
 
-    router->route("POST /session", [webdriver](Rc<Http::Request>, Rc<Http::Response::Writer>) -> Async::Task<> {
+    router->route("POST *.smnx.sh/assets/{path...}", [webdriver](Rc<Http::Request> req, Rc<Http::Response::Writer> resp) -> Async::Task<> {
         co_return Ok();
     });
 
-    router->route("DELETE /session/{id}", [webdriver](Rc<Http::Request>, Rc<Http::Response::Writer>) -> Async::Task<> {
+    router->route("DELETE /session/{id}", [webdriver](Rc<Http::Request> req, Rc<Http::Response::Writer> resp) -> Async::Task<> {
+        auto payload = co_trya$(req->readJsonAsync());
+        co_trya$(resp->writeJsonAsync(payload));
         co_return Ok();
     });
 
