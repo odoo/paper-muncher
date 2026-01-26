87dea07ffb6e9c74ce90581417e757c17b029981
sleepy-monax <me@smnx.sh>
2026-01-27 00:14:56 +0100

    meta: Bump karm and cutekit.

--- meta/plugins/__init__.py
+++ meta/plugins/__init__.py
@@ -1,6 +1,6 @@
 from cutekit import ensure
 
-ensure((0, 10, 0))
+ensure((0, 11, 0))
 
 from . import tools, wpt  # noqa E402, F401: Needed for side effect
 from . import reftests  # noqa E402, F401: Needed for side effect
--- src/vaev-script/main/main.cpp
+++ src/vaev-script/main/main.cpp
@@ -1,4 +1,4 @@
-#include <karm-sys/entry.h>
+#include <karm/entry>
 
 import Vaev.Script;
 import Karm.Core;
