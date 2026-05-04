caec907703675b5446f06ca23ded2ca94182ad93
sleepy-monax <me@smnx.sh>
2026-05-04 13:29:07 +0200

    paper-muncher: Added `--batch` option and added more doc in `--help`.

--- src/mod.cpp
+++ src/mod.cpp
@@ -20,8 +20,19 @@ import Karm.Ref;
 import Vaev.Engine;
 
 using namespace Karm;
+using namespace Karm::Literals;
+using namespace Karm::Math::Literals;
+using namespace Karm::Fmt::Literals;
 
 namespace PaperMuncher {
+
+//< What to do when multiple document are passed as input.
+export enum struct Batch {
+    CONCAT,   //< Concat them as a single document.
+    SEPARATE, //< Save them as separate document.
+
+    _LEN,
+};
 
 export enum struct Flow {
     AUTO,       //< Paginate for PDF, otherwise continuous
@@ -68,47 +79,105 @@ export struct Option {
     Opt<Vaev::Length> width = NONE;
     Opt<Vaev::Length> height = NONE;
     Opt<Vaev::Color> background = NONE;
-    Print::PaperStock paper = Print::A4;
+    Print::PaperStock stock = Print::A4;
     Print::Orientation orientation = Print::Orientation::PORTRAIT;
     Print::Margins margins = Print::Margins::DEFAULT;
     Ref::Uti outputFormat = Ref::Uti::PUBLIC_DATA;
+    Batch batch = Batch::CONCAT;
     Flow flow = Flow::AUTO;
     Extend extend = Extend::CROP;
 
-    auto preparePrintSettings() -> Print::Settings {
+    auto derivePrintSettings() const -> Print::Settings {
         Vaev::Layout::Resolver resolver;
-        resolver.viewport.dpi = this->scale;
+        resolver.viewport.dpi = this->density;
 
-        auto paper = this->paper;
-
-        if (this->orientation == Print::Orientation::LANDSCAPE)
-            paper = paper.landscape();
-
-        if (this->width or this->height) {
-            paper.name = "custom";
-            if (this->width)
-                paper.width = resolver.resolve(*this->width).cast<f64>();
-
-            if (this->height)
-                paper.height = resolver.resolve(*this->height).cast<f64>();
-        }
+        auto stock = this->stock;
+        if (this->width or this->height)
+            stock = Print::PaperStock::custom(
+                this->width ? resolver.resolve(*this->width) : stock.minorAxis,
+                this->height ? resolver.resolve(*this->height) : stock.majorAxis
+            );
 
         return {
-            .paper = paper,
+            .stock = stock,
+            .orientation = this->orientation,
             .margins = this->margins,
             .scale = this->scale.toDppx(),
         };
     }
 
-    Vaev::Style::Media prepareMedia() {
+    Vaev::Style::Media deriveMedia() {
         Vaev::Layout::Resolver resolver;
         auto width = this->width ? resolver.resolve(*this->width) : 800_au;
         auto height = this->height ? resolver.resolve(*this->height) : 600_au;
-        return Vaev::Style::Media::forRender({width, height}, this->scale);
+        return Vaev::Style::Media::forRender(
+            {width, height},
+            this->scale
+        );
     }
 };
 
-export Async::Task<> runAsync(
+Async::Task<> runSingleAsync(
+    Rc<Http::Client> client,
+    Ref::Url const& input,
+    Print::Printer& output,
+    Option options,
+    Async::CancellationToken ct
+) {
+    logInfo("loading {}...", input);
+    auto window = Vaev::Dom::Window::create(client);
+    co_trya$(window->loadLocationAsync(input, Ref::Uti::PUBLIC_OPEN, ct));
+
+    logInfo("rendering {}...", input);
+    if (options.flow == Flow::PAGINATE) {
+        auto settings = options.derivePrintSettings();
+        window->print(settings) | ForEach([&](Print::Page& page) {
+            page.print(
+                output,
+                {.showBackgroundGraphics = true}
+            );
+        });
+    } else {
+        auto media = options.deriveMedia();
+        window->changeMedia(media);
+
+        auto scene = window->render();
+
+        if (options.background.has()) {
+            scene = makeRc<Scene::Clear>(scene, Vaev::resolve(*options.background, Gfx::ALPHA));
+        }
+
+        // NOTE: Override the background of HTML document, since no
+        //       one really expect a html document to be transparent
+        else if (window->document()->documentElement()->namespaceUri() == Vaev::Html::NAMESPACE) {
+            scene = makeRc<Scene::Clear>(scene, Gfx::WHITE);
+        }
+
+        Math::Vec2Au size{
+            media.width,
+            media.height,
+        };
+
+        if (options.extend == Extend::FIT) {
+            auto overflow = window->ensureRender().frag->scrollableOverflow();
+            size.width = overflow.width;
+            size.height = overflow.height;
+        }
+
+        Print::Page page = {
+            size.cast<f64>(),
+            scene,
+        };
+        page.print(
+            output,
+            {.showBackgroundGraphics = true}
+        );
+    }
+
+    co_return Ok();
+}
+
+export Async::Task<> runBatchAsync(
     Rc<Http::Client> client,
     Vec<Ref::Url> const& inputs,
     Ref::Url const& output,
@@ -121,75 +190,53 @@ export Async::Task<> runAsync(
                 ? Flow::PAGINATE
                 : Flow::CONTINUOUS;
 
-    auto printer = co_try$(
-        Print::FilePrinter::create(
-            options.outputFormat,
-            {
-                .density = options.density.toDppx(),
-            }
-        )
-    );
-
-    for (auto& input : inputs) {
-        logInfo("loading {}...", input);
-        auto window = Vaev::Dom::Window::create(client);
-        co_trya$(window->loadLocationAsync(input, Ref::Uti::PUBLIC_OPEN, ct));
-
-        logInfo("rendering {}...", input);
-        if (options.flow == Flow::PAGINATE) {
-            auto settings = options.preparePrintSettings();
-            window->print(settings) | ForEach([&](Print::Page& page) {
-                page.print(
-                    *printer,
-                    {.showBackgroundGraphics = true}
-                );
-            });
-        } else {
-            auto media = options.prepareMedia();
-            window->changeMedia(media);
-
-            auto scene = window->render();
-
-            if (options.background.has()) {
-                scene = makeRc<Scene::Clear>(scene, Vaev::resolve(*options.background, Gfx::ALPHA));
-            }
-            // NOTE: Override the background of HTML document, since no
-            //       one really expect a html document to be transparent
-            else if (window->document()->documentElement()->namespaceUri() == Vaev::Html::NAMESPACE) {
-                scene = makeRc<Scene::Clear>(scene, Gfx::WHITE);
-            }
-
-            Print::PaperStock paper{
-                "image",
-                media.width.cast<f64>(),
-                media.height.cast<f64>(),
-            };
-
-            if (options.extend == Extend::FIT) {
-                auto overflow = window->ensureRender().frag->scrollableOverflow();
-                paper.width = overflow.width.cast<f64>();
-                paper.height = overflow.height.cast<f64>();
-            }
-
-            Print::Page page = {paper, scene};
-            page.print(
-                *printer,
-                {.showBackgroundGraphics = true}
+    if (options.batch == Batch::CONCAT) {
+        auto printer = co_try$(
+            Print::FilePrinter::create(
+                options.outputFormat,
+                {
+                    .density = options.density.toDppx(),
+                }
+            )
+        );
+        for (auto& input : inputs)
+            co_trya$(runSingleAsync(client, input, *printer, options, ct));
+        logInfo("saving {}...", output);
+        Io::BufferWriter bw;
+        co_try$(printer->write(bw));
+        co_trya$(client->doAsync(
+            Http::Request::from(
+                Http::Method::PUT,
+                output,
+                Http::Body::from(bw.take())
+            ),
+            ct
+        ));
+    } else {
+        for (auto& input : inputs) {
+            auto fileUrl = output / "{}.{}"_f(input.path.stem(), options.outputFormat.primarySuffix());
+            auto printer = co_try$(
+                Print::FilePrinter::create(
+                    options.outputFormat,
+                    {
+                        .density = options.density.toDppx(),
+                    }
+                )
             );
+            co_trya$(runSingleAsync(client, input, *printer, options, ct));
+            logInfo("saving {}...", fileUrl);
+            Io::BufferWriter bw;
+            co_try$(printer->write(bw));
+            co_trya$(client->doAsync(
+                Http::Request::from(
+                    Http::Method::PUT,
+                    fileUrl,
+                    Http::Body::from(bw.take())
+                ),
+                ct
+            ));
         }
     }
-
-    logInfo("saving {}...", output);
-    Io::BufferWriter bw;
-    co_try$(printer->write(bw));
-    co_trya$(client->doAsync(
-        Http::Request::from(
-            Http::Method::PUT,
-            output,
-            Http::Body::from(bw.take())
-        ),
-        ct
-    ));
 
     co_return Ok();
 }
