2e9581e0d33e69b650f2729248bf24df1c2e936f
sleepy-monax <me@smnx.sh>
2026-08-12 10:47:12 +0200

    paper-muncher: Fix landlock code build on kernel before v7.

--- src/paper-muncher/linux/landlock.cpp
+++ src/paper-muncher/linux/landlock.cpp
@@ -1,9 +1,12 @@
 module;
 
 #include <fcntl.h>
-#include <linux/landlock.h>
 #include <sys/syscall.h>
 #include <unistd.h>
+
+// NOTE: This file had to be vendored because older version of the kernel
+//       headers doesn't have all the declaration for newer ABIs.
+#include "landlock.h"
 
 export module PaperMuncher.Linux:landlock;
 
@@ -16,43 +19,13 @@ using namespace Karm;
 
 namespace PaperMuncher {
 
-#ifndef LANDLOCK_ACCESS_FS_REFER
-#    define LANDLOCK_ACCESS_FS_REFER 0
-#endif
-
-#ifndef LANDLOCK_ACCESS_FS_TRUNCATE
-#    define LANDLOCK_ACCESS_FS_TRUNCATE 0
-#endif
-
-#ifndef LANDLOCK_ACCESS_NET_BIND_TCP
-#    define LANDLOCK_ACCESS_NET_BIND_TCP 0
-#endif
-
-#ifndef LANDLOCK_ACCESS_NET_BIND_TCP
-#    define LANDLOCK_ACCESS_NET_BIND_TCP 0
-#    define LANDLOCK_ACCESS_NET_CONNECT_TCP 0
-#endif
-
-#ifndef LANDLOCK_ACCESS_FS_IOCTL_DEV
-#    define LANDLOCK_ACCESS_FS_IOCTL_DEV 0
-#endif
-
-#ifndef LANDLOCK_SCOPE_ABSTRACT_UNIX_SOCKET
-#    define LANDLOCK_SCOPE_ABSTRACT_UNIX_SOCKET 0
-#    define LANDLOCK_SCOPE_SIGNAL 0
-#endif
-
-#ifndef LANDLOCK_ACCESS_FS_RESOLVE_UNIX
-#    define LANDLOCK_ACCESS_FS_RESOLVE_UNIX 0
-#endif
-
-#ifndef LANDLOCK_ACCESS_NET_BIND_UDP
-#    define LANDLOCK_ACCESS_NET_BIND_UDP 0
-#    define LANDLOCK_ACCESS_NET_CONNECT_SEND_UDP 0
-#endif
-
 export Res<> landlockAllowReadingDirectory(Vec<Str> dirs) {
-    int abi = syscall(__NR_landlock_create_ruleset, NULL, 0, LANDLOCK_CREATE_RULESET_VERSION);
+    int const abi = syscall(
+        __NR_landlock_create_ruleset,
+        NULL,
+        0,
+        LANDLOCK_CREATE_RULESET_VERSION
+    );
 
     if (abi < 0) {
         logWarn("landlock is disabled on this kernel (abi:{})", abi);
@@ -94,34 +67,44 @@ export Res<> landlockAllowReadingDirectory(Vec<Str> dirs) {
     case 1:
         /* Removes LANDLOCK_ACCESS_FS_REFER for ABI < 2 */
         attr.handled_access_fs &= ~LANDLOCK_ACCESS_FS_REFER;
-        __attribute__((fallthrough));
+        [[fallthrough]];
+
     case 2:
         /* Removes LANDLOCK_ACCESS_FS_TRUNCATE for ABI < 3 */
         attr.handled_access_fs &= ~LANDLOCK_ACCESS_FS_TRUNCATE;
-        __attribute__((fallthrough));
+        [[fallthrough]];
+
     case 3:
         /* Removes network support for ABI < 4 */
         attr.handled_access_net &=
             ~(LANDLOCK_ACCESS_NET_BIND_TCP |
               LANDLOCK_ACCESS_NET_CONNECT_TCP);
-        __attribute__((fallthrough));
+        [[fallthrough]];
+
     case 4:
         /* Removes LANDLOCK_ACCESS_FS_IOCTL_DEV for ABI < 5 */
         attr.handled_access_fs &= ~LANDLOCK_ACCESS_FS_IOCTL_DEV;
-        __attribute__((fallthrough));
+        [[fallthrough]];
+
     case 5:
         /* Removes LANDLOCK_SCOPE_* for ABI < 6 */
         attr.scoped &= ~(LANDLOCK_SCOPE_ABSTRACT_UNIX_SOCKET | LANDLOCK_SCOPE_SIGNAL);
-        __attribute__((fallthrough));
+        [[fallthrough]];
+
     case 6 ... 8:
         /* Removes LANDLOCK_ACCESS_FS_RESOLVE_UNIX for ABI < 9 */
         attr.handled_access_fs &= ~LANDLOCK_ACCESS_FS_RESOLVE_UNIX;
-        __attribute__((fallthrough));
+        [[fallthrough]];
+
     case 9:
         /* Removes LANDLOCK_ACCESS_NET_*_UDP for ABI < 10 */
         attr.handled_access_net &=
             ~(LANDLOCK_ACCESS_NET_BIND_UDP |
               LANDLOCK_ACCESS_NET_CONNECT_SEND_UDP);
+        [[fallthrough]];
+
+    default:
+        break;
     }
 
     int rulesetFd = syscall(__NR_landlock_create_ruleset, &attr, sizeof(attr), 0);
